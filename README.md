859df0e74fd5390e46db32ca567f8c5aa50b77b0
Corncycle <caleb.mes@gmail.com>
2023-07-06 18:14:18 -0700

    Set up initial dependencies

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # blog-client
+
 Coming soon!
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => {
+  return <h1>Hello world! This page was renderesdfd in Reacasdft!</h1>
+}
+
+export default App
